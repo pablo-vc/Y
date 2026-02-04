4a786f6bf371dbc5389f60d063d9e35c7b183383
Pablo <pablo.vaz.111@gmail.com>
2026-02-04 09:17:33 +0100

    validación de datos de inicio de sesión y registro

--- Moviles/Y/app/src/main/java/com/example/y/ui/login/LogInActivity.java
+++ Moviles/Y/app/src/main/java/com/example/y/ui/login/LogInActivity.java
@@ -1,6 +1,11 @@
 package com.example.y.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.y.MainActivity;
 import com.example.y.R;
+import com.example.y.databinding.FragmentHomeBinding;
 
 public class LogInActivity extends AppCompatActivity {
+    EditText etUser, etPassword;
+    Button btnLogin;
+    TextView tvRegister, tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,54 @@ public class LogInActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            initUI();
+            setUpListeners();
             return insets;
+
         });
     }
+
+    private void initUI() {
+        btnLogin = findViewById(R.id.btnLogin);
+        etUser=findViewById(R.id.etUser);
+        etPassword=findViewById(R.id.etPassword);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+        tvRegister = findViewById(R.id.tvRegister);
+    }
+
+    private void setUpListeners() {
+        btnLogin.setOnClickListener(v -> {
+            loginManagement();
+        });
+        tvRegister.setOnClickListener(v -> {
+            startActivity(new Intent(LogInActivity.this, RegisterActivity.class));
+        });
+        tvForgotPassword.setOnClickListener(v -> {
+            startActivity(new Intent(LogInActivity.this, ForgotPasswordActivity.class));
+        });
+    }
+
+    private void loginManagement() {
+        Boolean valid = false;
+        if (validation()) {
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private Boolean validation() {
+        if (etUser.getText() != null && etUser.getText().toString().trim().length() == 0) {
+            etUser.setError("Username is required");
+            etUser.requestFocus();
+            return false;
+        }
+        if (etPassword.getText() != null && etPassword.getText().toString().trim().length() == 0) {
+            etPassword.setError("Password is required");
+            etUser.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
 }
