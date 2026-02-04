f75a69909ad92604023ee98b8d306660ea7c265c
Pablo <pablo.vaz.111@gmail.com>
2026-02-04 09:17:33 +0100

    validación de datos de inicio de sesión y registro

--- Moviles/Y/app/src/main/java/com/example/y/ui/login/RegisterActivity.java
+++ Moviles/Y/app/src/main/java/com/example/y/ui/login/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.y.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.y.MainActivity;
 import com.example.y.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etUser, etEmail, etPassword,etPassword2;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,4 +31,63 @@ public class RegisterActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private void initUI() {
+        btnRegister = findViewById(R.id.btnRegister);
+        etUser = findViewById(R.id.etUser);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etPassword2 = findViewById(R.id.etPassword2);
+    }
+
+    private void setUpListeners() {
+        btnRegister.setOnClickListener(v -> {
+            loginManagement();
+        });
+
+    }
+
+    private void loginManagement() {
+        Boolean valid = false;
+        if (validation()) {
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private Boolean validation() {
+        if (etUser.getText() != null && etUser.getText().toString().trim().length() == 0) {
+            etUser.setError("Username is required");
+            etUser.requestFocus();
+            return false;
+        }
+        if (etPassword.getText() != null && etPassword.getText().toString().trim().length() == 0) {
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+            return false;
+        }
+        if (etPassword2.getText() != null && etPassword2.getText().toString().trim().length() == 0) {
+            etPassword2.setError("Password is required");
+            etPassword2.requestFocus();
+            return false;
+        }
+        if (etPassword.getText().toString().trim().length()<6){
+            etPassword.setError("Password must have at least 6 characters");
+            etPassword.requestFocus();
+            return false;
+        }
+        if (etPassword.getText()!=etPassword2.getText()){
+            etPassword2.setError("The passwords are different");
+            etPassword2.requestFocus();
+            return false;
+        }
+
+        if (etEmail.getText() != null && !Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString().trim()).matches()) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
