31e8a2480b7f73ce62344770f8f4713d70644b59
Pablo <pablo.vaz.111@gmail.com>
2026-02-10 11:59:53 +0100

    profile ui updated

--- Moviles/Y/app/src/main/java/com/example/y/ui/profile/ProfileFragment.java
+++ Moviles/Y/app/src/main/java/com/example/y/ui/profile/ProfileFragment.java
@@ -1,17 +1,15 @@
 package com.example.y.ui.profile;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.y.Post;
-import com.example.y.R;
 import com.example.y.adapters.PostAdapter;
 import com.example.y.databinding.FragmentProfileBinding;
 import com.google.android.material.tabs.TabLayout;
